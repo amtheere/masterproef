70fbc5268fd2301013b3aa3163b0dd46b8733634
Adnan Theerens <adnan.theerens@ugent.be>
2021-04-09 15:38:43 +0200

    synthetic dataset

--- experiment/synthetic_dataset.py
+++ experiment/synthetic_dataset.py
@@ -6,101 +6,145 @@ from latextable import draw_latex
 from pyod.models.lof import LOF
 from scipy.stats import skewnorm
 
-
 mean_class_1 = [-2, 0]
 mean_class_2 = [2, 0]
-var_class_1 = [0.6, 0.2]
-var_class_2 = [0.2, 0.1]
+sigma_1 = 0.6
+sigma_2 = 0.2
+var_class_1 = [sigma_1, 0.2]
+var_class_2 = [sigma_2, 0.1]
+test_samples_per_class = 500
 number_outliers_1 = 30
 number_outliers_2 = 30
+ratio_outlier_1 = number_outliers_1 / (100 + number_outliers_1)
+ratio_outlier_2 = number_outliers_2 / (100 + number_outliers_2)
+samples_class_1_outliers = round(test_samples_per_class * ratio_outlier_1)
+samples_class_1 = test_samples_per_class - samples_class_1_outliers
+samples_class_2_outliers = round(test_samples_per_class * ratio_outlier_2)
+samples_class_2 = test_samples_per_class - samples_class_2_outliers
+samples_normal = samples_class_1 + samples_class_2
+samples_outliers = samples_class_1_outliers + samples_class_2_outliers
 alpha_1 = 2
 omega_1 = 1.5
 alpha_2 = -2
 omega_2 = 0.7
-ratio_outlier_1 = number_outliers_1/(100+number_outliers_1)
-ratio_outlier_2 = number_outliers_2/(100+number_outliers_2)
-results = []
-times_the_best = list(np.zeros(9, dtype=int))
-
-rv = skewnorm(5, scale=2, loc=-2)
-x = np.linspace(-4, 10, 1000)
-plt.plot(x, rv.pdf(x))
-plt.show()
-
-for i in range(50):
-    np.random.seed(i)
-    class_1 = np.random.normal(mean_class_1, var_class_1, (100, 2))
-    normal_class_1 = np.random.normal(mean_class_1[1], var_class_1[1], number_outliers_1)
-    skew_class_1 = skewnorm.rvs(alpha_1, size=number_outliers_1, scale=omega_1, loc=mean_class_1[0])
-    class_1_outliers = np.array([np.array([x, y]) for x, y in zip(skew_class_1, normal_class_1)])
-
-    class_2 = np.random.normal(mean_class_2, var_class_2, (100, 2))
-    normal_class_2 = np.random.normal(mean_class_2[1], var_class_2[1], number_outliers_2)
-    skew_class_2 = skewnorm.rvs(alpha_2, size=number_outliers_2, scale=omega_2, loc=mean_class_2[0])
-    class_2_outliers = np.array([np.array([x, y]) for x, y in zip(skew_class_2, normal_class_2)])
-
-    cl1 = plt.scatter(class_1[:, 0], class_1[:, 1], c="blue")
-    cl1_o = plt.scatter(class_1_outliers[:, 0], class_1_outliers[:, 1], c="purple", marker="<")
-    cl2 = plt.scatter(class_2[:, 0], class_2[:, 1], c="red")
-    cl2_o = plt.scatter(class_2_outliers[:, 0], class_2_outliers[:, 1], c="orange", marker=">")
-    plt.legend((cl1, cl1_o, cl2, cl2_o),
-               ("class 1", "class 1 outliers", "class 2", "class 2 outliers"),
-               scatterpoints=1,
-               loc='lower left',
-               ncol=2,
-               fontsize=8)
-    plt.axis([-4, 4, -1, 1])
-    plt.show()
-    x_train = np.concatenate((class_1, class_1_outliers, class_2, class_2_outliers))
-    y_train = np.append(np.zeros(100 + number_outliers_1), np.ones(100 + number_outliers_2)).astype(int)
-
-    test_samples_per_class = 500
-    samples_class_1_outliers = round(test_samples_per_class*ratio_outlier_1)
-    samples_class_1 = test_samples_per_class-samples_class_1_outliers
-    samples_class_2_outliers = round(test_samples_per_class * ratio_outlier_2)
-    samples_class_2 = test_samples_per_class - samples_class_2_outliers
-    class_1 = np.random.normal(mean_class_1, var_class_1, (samples_class_1, 2))
-    normal_class_1 = np.random.normal(mean_class_1[1], var_class_1[1], samples_class_1_outliers)
-    skew_class_1 = skewnorm.rvs(alpha_1, size=samples_class_1_outliers, scale=omega_1, loc=mean_class_1[0])
-    class_1_outliers = np.array([np.array([x, y]) for x, y in zip(skew_class_1, normal_class_1)])
-    class_2 = np.random.normal(mean_class_2, var_class_2, (samples_class_2, 2))
-    normal_class_2 = np.random.normal(mean_class_2[1], var_class_2[1], samples_class_2_outliers)
-    skew_class_2 = skewnorm.rvs(alpha_2, size=samples_class_2_outliers, scale=omega_2, loc=mean_class_2[0])
-    class_2_outliers = np.array([np.array([x, y]) for x, y in zip(skew_class_2, normal_class_2)])
-    cl1 = plt.scatter(class_1[:, 0], class_1[:, 1], c="blue")
-    cl1_o = plt.scatter(class_1_outliers[:, 0], class_1_outliers[:, 1], c="purple", marker="<")
-    cl2 = plt.scatter(class_2[:, 0], class_2[:, 1], c="red")
-    cl2_o = plt.scatter(class_2_outliers[:, 0], class_2_outliers[:, 1], c="orange", marker=">")
-    plt.legend((cl1, cl1_o, cl2, cl2_o),
-               ("class 1", "class 1 outliers", "class 2", "class 2 outliers"),
-               scatterpoints=1,
-               loc='lower left',
-               ncol=2,
-               fontsize=8)
-    plt.axis([-4, 4, -1, 1])
-    plt.show()
-    x_test = np.concatenate((class_1, class_1_outliers, class_2, class_2_outliers))
-    y_test = np.append(np.zeros(test_samples_per_class), np.ones(test_samples_per_class)).astype(int)
-    acc = accuracy_test(x_train, y_train, x_test, y_test, outlierScoreAlgorithm=LOF(contamination=0.15))
-    maximum = np.amax(acc)
-    for j in range(9):
-        if acc[j] == maximum:
-            times_the_best[j] += 1
-    table = Texttable()
-    table.add_row(["Min", "Mino", "FR", "Avg", "Avgo", "TS", "OWA", "OWAo", "WOWA"])
-    table.add_row(acc)
-    print(table.draw() + "\n")
-    results.append(acc)
-
-mean_accuracy = list(np.mean(results, axis=0))
-mean_accuracy.insert(0, "Average")
-median_accuracy = list(np.median(results, axis=0))
-median_accuracy.insert(0, "Median")
-times_the_best.insert(0, "times the best")
 table = Texttable()
-table.add_row(["Summary", "Min", "Mino", "FR", "Avg", "Avgo", "TS", "OWA", "OWAo", "WOWA"])
-table.add_row(mean_accuracy)
-table.add_row(median_accuracy)
-table.add_row(times_the_best)
-print(table.draw() + "\n")
-print(draw_latex(table) + "\n")
+table.add_row(["Summary", "Min", "Mino", "FR", "Avg", "Avgo", "TS", "OWA", "OWAo", "WOWA", "Value"])
+median_accuracy = []
+median_accuracy_outliers = []
+
+start = 0.1
+steps = 12
+step_size = 0.1
+step = [0, 0.1, 0.2, 0.3, 0.5, 0.7, 0.9, 1.2, 1.5, 2, 2.5, 3.5, 4]
+parameter_name = "alpha_2"
+if __name__ == "__main__":
+    for k in range(len(step)):
+        alpha_2 = -step[k]
+        results_normal = []
+        results_outlier = []
+        results = []
+        times_the_best = list(np.zeros(9, dtype=int))
+        times_the_best_outliers = list(np.zeros(9, dtype=int))
+        for i in range(40):
+            np.random.seed(i+40*k)
+            class_1_train = np.random.normal(mean_class_1, var_class_1, (100, 2))
+            normal_class_1 = np.random.normal(mean_class_1[1], var_class_1[1], number_outliers_1)
+            skew_class_1 = skewnorm.rvs(alpha_1, size=number_outliers_1, scale=omega_1, loc=mean_class_1[0])
+            class_1_outliers_train = np.array([np.array([x, y]) for x, y in zip(skew_class_1, normal_class_1)])
+
+            class_2_train = np.random.normal(mean_class_2, var_class_2, (100, 2))
+            normal_class_2 = np.random.normal(mean_class_2[1], var_class_2[1], number_outliers_2)
+            skew_class_2 = skewnorm.rvs(alpha_2, size=number_outliers_2, scale=omega_2, loc=mean_class_2[0])
+            class_2_outliers_train = np.array([np.array([x, y]) for x, y in zip(skew_class_2, normal_class_2)])
+            x_train = np.concatenate((class_1_train, class_1_outliers_train, class_2_train, class_2_outliers_train))
+            y_train = np.append(np.zeros(100 + number_outliers_1), np.ones(100 + number_outliers_2)).astype(int)
+
+            class_1_test = np.random.normal(mean_class_1, var_class_1, (samples_class_1, 2))
+            class_2_test = np.random.normal(mean_class_2, var_class_2, (samples_class_2, 2))
+            x_test = np.concatenate((class_1_test, class_2_test))
+            y_test = np.append(np.zeros(samples_class_1), np.ones(samples_class_2)).astype(int)
+            acc_normal = accuracy_test(x_train, y_train, x_test, y_test,
+                                       outlierScoreAlgorithm=LOF(contamination=ratio_outlier_1))
+            results_normal.append(acc_normal)
+
+            normal_class_1 = np.random.normal(mean_class_1[1], var_class_1[1], samples_class_1_outliers)
+            skew_class_1 = skewnorm.rvs(alpha_1, size=samples_class_1_outliers, scale=omega_1, loc=mean_class_1[0])
+            class_1_outliers_test = np.array([np.array([x, y]) for x, y in zip(skew_class_1, normal_class_1)])
+            normal_class_2 = np.random.normal(mean_class_2[1], var_class_2[1], samples_class_2_outliers)
+            skew_class_2 = skewnorm.rvs(alpha_2, size=samples_class_2_outliers, scale=omega_2, loc=mean_class_2[0])
+            class_2_outliers_test = np.array([np.array([x, y]) for x, y in zip(skew_class_2, normal_class_2)])
+            x_test = np.concatenate((class_1_outliers_test, class_2_outliers_test))
+            y_test = np.append(np.zeros(samples_class_1_outliers), np.ones(samples_class_2_outliers)).astype(int)
+            acc_outliers = accuracy_test(x_train, y_train, x_test, y_test,
+                                         outlierScoreAlgorithm=LOF(contamination=ratio_outlier_1))
+            results_outlier.append(acc_outliers)
+            acc = [(samples_normal * x + samples_outliers * y) / (2 * test_samples_per_class)
+                   for (x, y) in zip(acc_normal, acc_outliers)]
+            results.append(acc)
+
+            if i == 0:
+                cl1 = plt.scatter(class_1_train[:, 0], class_1_train[:, 1], c="blue")
+                cl1_o = plt.scatter(class_1_outliers_train[:, 0], class_1_outliers_train[:, 1], c="purple", marker="<")
+                cl2 = plt.scatter(class_2_train[:, 0], class_2_train[:, 1], c="red")
+                cl2_o = plt.scatter(class_2_outliers_train[:, 0], class_2_outliers_train[:, 1], c="orange", marker=">")
+                plt.legend((cl1, cl1_o, cl2, cl2_o),
+                           ("class 1", "class 1 outliers", "class 2", "class 2 outliers"),
+                           scatterpoints=1,
+                           loc='lower left',
+                           ncol=2,
+                           fontsize=8)
+                plt.axis([-4, 4, -1, 1])
+                plt.savefig("plots/"+parameter_name+"_plots/train/"+str(step[k])+".png")
+                plt.clf()
+                cl1 = plt.scatter(class_1_test[:, 0], class_1_test[:, 1], c="blue")
+                cl1_o = plt.scatter(class_1_outliers_test[:, 0], class_1_outliers_test[:, 1], c="purple", marker="<")
+                cl2 = plt.scatter(class_2_test[:, 0], class_2_test[:, 1], c="red")
+                cl2_o = plt.scatter(class_2_outliers_test[:, 0], class_2_outliers_test[:, 1], c="orange", marker=">")
+                plt.legend((cl1, cl1_o, cl2, cl2_o),
+                           ("class 1", "class 1 outliers", "class 2", "class 2 outliers"),
+                           scatterpoints=1,
+                           loc='lower left',
+                           ncol=2,
+                           fontsize=8)
+                plt.axis([-4, 4, -1, 1])
+                plt.savefig("plots/"+parameter_name+"_plots/test/"+str(step[k])+".png")
+                plt.clf()
+
+            '''
+            maximum = np.amax(acc_outliers)
+            for j in range(9):
+                if acc_outliers[j] == maximum:
+                    times_the_best_outliers[j] += 1
+            maximum = np.amax(acc)
+            for j in range(9):
+                if acc[j] == maximum:
+                    times_the_best[j] += 1
+            '''
+
+        # mean_accuracy = list(np.mean(results, axis=0))
+        # mean_accuracy.insert(0, "Average")
+        median_accuracy.append(list(np.median(results, axis=0)))
+        results = list(np.median(results, axis=0))
+        results.insert(0, "Median")
+        results.append(start+k*step_size)
+        # times_the_best.insert(0, "times the best")
+        # mean_accuracy_outliers = list(np.mean(results_outlier, axis=0))
+        # mean_accuracy_outliers.insert(0, "Average")
+        median_accuracy_outliers.append(list(np.median(results_outlier, axis=0)))
+        results_outliers = list(np.median(results_outlier, axis=0))
+        results_outliers.insert(0, "Median")
+        results_outliers.append(start+k*step_size)
+        # times_the_best_outliers.insert(0, "times the best")
+        # table.add_row(mean_accuracy)
+        table.add_row(results)
+        # table.add_row(times_the_best)
+        # table.add_row(mean_accuracy_outliers)
+        table.add_row(results_outliers)
+        # table.add_row(times_the_best_outliers)
+    x = [start + k * step_size for k in range(steps)]
+    median_accuracy = np.array(median_accuracy)
+    median_accuracy_outliers = np.array(median_accuracy_outliers)
+    np.savetxt("results_synth/median_"+parameter_name+".csv", median_accuracy, delimiter=", ")
+    np.savetxt("results_synth/median_"+parameter_name+"_outliers.csv", median_accuracy_outliers, delimiter=", ")
+    print(table.draw() + "\n")
+    print(draw_latex(table) + "\n")
---
+++ experiment/synthetic_plot.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.rcParams['text.usetex'] = True
+
+
+# start van o_1 = o_2 = 5 stappen van 5 default 30
+# start van omega_1 = 1 stappen van 0.1 default 1.5
+# start van omega_2 = 0.5 stappen van 0.1 default 0.7
+# start van sigma_1 = 0.2 stappen van 0.1 default 0.6
+# start van sigma_2 = 0.1 stappen van 0.1 default 0.2
+# steps van alpha_1 en 2 [0, 0.1, 0.2, 0.3, 0.5, 0.7, 0.9, 1.2, 1.5, 2, 2.5, 3.5, 4]
+
+start = 0.1
+step_size = 0.1
+step = [0, 0.1, 0.2, 0.3, 0.5, 0.7, 0.9, 1.2, 1.5, 2, 2.5, 3.5, 4]
+parameter_name = "alpha_2"
+median_accuracy = np.genfromtxt("results_synth/median_" + parameter_name + ".csv", delimiter=", ")
+x = step
+# x = [start + k * step_size for k in range(len(median_accuracy))]
+mini = median_accuracy[:, 0]
+mino = median_accuracy[:, 1]
+FR = median_accuracy[:, 2]
+avg = median_accuracy[:, 3]
+avgo = median_accuracy[:, 4]
+TS = median_accuracy[:, 5]
+OWA = median_accuracy[:, 6]
+OWAo = median_accuracy[:, 7]
+WOWA = median_accuracy[:, 8]
+min_plt = plt.plot(x, mini, alpha=0.5, ls="--", label="min", color='cyan')
+mino_plt = plt.plot(x, mino, alpha=0.5, ls="-", label="mino", color="midnightblue")
+FR_plt = plt.plot(x, FR, alpha=0.5, ls="-.", label="FR", color="darkmagenta")
+avg_plt = plt.plot(x, avg, alpha=0.5, ls="--", label="avg", color="greenyellow")
+avgo_plt = plt.plot(x, avgo, alpha=0.5, ls="-", label="avgo", color="darkgreen")
+TS_plt = plt.plot(x, TS, alpha=0.5, ls="-.", label="TS", color="darkslategrey")
+OWA_plt = plt.plot(x, OWA, alpha=0.5, ls="--", label="OWA", color="lightcoral")
+OWAo_plt = plt.plot(x, OWAo, alpha=0.5, ls="-", label="OWAo", color="darkred")
+WOWA_plt = plt.plot(x, WOWA, alpha=0.5, ls="-.", label='WOWA', color="orange")
+plt.axis(ymax=0.990)
+plt.xticks(x, rotation=45)
+plt.xlabel(r"$\alpha_2$")
+plt.ylabel('accuracy')
+plt.legend(ncol=3, loc="best")
+plt.savefig("graphs/" + parameter_name + ".png", dpi=300)
+plt.show()
+median_accuracy = np.genfromtxt("results_synth/median_" + parameter_name + "_outliers.csv", delimiter=", ")
+mini = median_accuracy[:, 0]
+mino = median_accuracy[:, 1]
+FR = median_accuracy[:, 2]
+avg = median_accuracy[:, 3]
+avgo = median_accuracy[:, 4]
+TS = median_accuracy[:, 5]
+OWA = median_accuracy[:, 6]
+OWAo = median_accuracy[:, 7]
+WOWA = median_accuracy[:, 8]
+min_plt = plt.plot(x, mini, alpha=0.5, ls="--", label="min", color='cyan')
+mino_plt = plt.plot(x, mino, alpha=0.5, ls="-", label="mino", color="midnightblue")
+FR_plt = plt.plot(x, FR, alpha=0.5, ls="-.", label="FR", color="darkmagenta")
+avg_plt = plt.plot(x, avg, alpha=0.5, ls="--", label="avg", color="greenyellow")
+avgo_plt = plt.plot(x, avgo, alpha=0.5, ls="-", label="avgo", color="darkgreen")
+TS_plt = plt.plot(x, TS, alpha=0.5, ls="-.", label="TS", color="darkslategrey")
+OWA_plt = plt.plot(x, OWA, alpha=0.5, ls="--", label="OWA", color="lightcoral")
+OWAo_plt = plt.plot(x, OWAo, alpha=0.5, ls="-", label="OWAo", color="darkred")
+WOWA_plt = plt.plot(x, WOWA, alpha=0.5, ls="-.", label='WOWA', color="orange")
+plt.legend(ncol=3, loc="best")
+plt.axis(ymax=0.96)
+plt.xticks(x, rotation=45)
+plt.xlabel(r"$\alpha_2$")
+plt.ylabel('accuracy')
+plt.savefig("graphs/" + parameter_name + "_outliers.png", dpi=300)
+plt.show()
